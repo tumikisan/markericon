41053d3c7537c03ca39bfeecafa4a0707d0cea53
tumikisan <masikinoaizi@gmail.com>
2025-08-17 02:09:59 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+const config = {
+  webAppUrl: "https://script.google.com/macros/s/AKfycbyPqYOEQNw918gM-HzEg6YgWUshDNI4-M_1_ymsUMz2N46zDHXuh-kTSNbizUQxnI0swQ/exec",
+  secretToken: "ljpobipu9ujjkjmchff89way98y9kjsfiun@9duy89b9zkjxbtechltchgh"
+};
