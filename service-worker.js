10d5350a7bd66d0db6857d85eea2089fc83d864b
tumikisan <masikinoaizi@gmail.com>
2026-03-20 17:00:25 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ importScripts('./db.js');
 
   // --- IndexedDBヘルパー関数ここまで ---
 ////index.html や db.js を変更したら、必ず sw.txt の CACHE_NAME のバージョンを上げてください（例: v4.1 → v4.2）。これが更新の引き金となります。
-const CACHE_NAME = 'map-app-cache-v10.9'; 
+const CACHE_NAME = 'map-app-cache-v11.0'; 
 
 // ★ オフラインで表示したいファイルのリスト
 const urlsToCache = [
